db7cbec25379f1ca5cae78c594c7fb31db15d188
Sean Clifford <sclifford6262@gmail.com>
2018-05-04 20:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  return {...driver, key: value}
+  driverClone = {...driver}
+  driverClone[key] = value
+  return driverClone
 }
